f51a662f338b70dac76a17f27e051b250bef7fd9
loic-roux <69loloro10@gmail.com>
2021-11-24 10:20:56 +0100

    linter conf

--- td/tp_03_dart_functions.dart
+++ td/tp_03_dart_functions.dart
@@ -1,38 +1,38 @@
 void main() {
-  
   // TODO : créer la fonction greeting qui retourne par exemple 'Bonjour, on est le 01/02/2019 il est 14h40'
   // TODO : Le deuxieme parametre est facultatif et prend une valeur true par defaut qui permet d'ajouter Bonjour au debut de la phrase
-  
-  DateTime currentDate = DateTime.now();
-  String myGreetingResult = greeting(date: currentDate);
+
+  var currentDate = DateTime.now();
+  var myGreetingResult = greeting(date: currentDate);
   print(myGreetingResult);
 
-  bool sayHello = false;
-  String myGreetingResult2 = greeting(date: currentDate, sayHello: sayHello);
-
+  var sayHello = false;
+  var myGreetingResult2 = greeting(date: currentDate, sayHello: sayHello);
+  print(myGreetingResult2);
 }
 
 void main2() {
-  
-  // TODO : modifier le fonction greeting afin d'avoir des parametres comme suit  
-  DateTime currentDate = DateTime.now();
-  String myGreetingResult = greeting(date : currentDate);
+  // TODO : modifier le fonction greeting afin d'avoir des parametres comme suit
+  var currentDate = DateTime.now();
+  var myGreetingResult = greeting(date: currentDate);
+  print(myGreetingResult);
 
-  bool sayHello = false;
-  String myGreetingResult2 = greeting(date : currentDate, sayHello: sayHello);
-
+  var sayHello = false;
+  var myGreetingResult2 = greeting(date: currentDate, sayHello: sayHello);
+  print(myGreetingResult2);
 }
 
-
 void main3() {
-  int theSecretNumber = 7;
+  var theSecretNumber = 7;
 
   void ckeckTheElement(int element) {
-    print(element == theSecretNumber ? 'YES THIS IS THE ONE ! $element' : 'IT\'S NOT $element GOOD LUCK NEXT TIME'); 
+    print(element == theSecretNumber
+        ? 'YES THIS IS THE ONE ! $element'
+        : 'IT\'S NOT $element GOOD LUCK NEXT TIME');
   }
 
   // TODO 1 : Parcourir la liste et executer ckeckTheElement pour chaque element de la list
-  List<int> lotoList = [1, 2, 3, 49, 7, 39, 2];
+  var lotoList = <int>[1, 2, 3, 49, 7, 39, 2];
   for (var el in lotoList) {
     ckeckTheElement(el);
   }
@@ -40,13 +40,11 @@ void main3() {
   // TODO 2 : Remplir la liste avec des nombres entiers aleatoire compris entre 0 et 10 (Créer une fonction generateRandomNumber)
 
   // TODO 3 : supprimer la fonction ckeckTheElement et utiliser une fonction anonyme a la place
-
 }
 
 void main4() {
-
-  aVoidFunction(int element) {
-    print('this is simply a void function'); 
+  String aVoidFunction(int element) {
+    return 'this is simply a void function';
   }
 
   print(aVoidFunction(2));
@@ -56,6 +54,6 @@ void main4() {
 String greeting({required DateTime date, sayHello = false}) {
   var res = sayHello ? 'Bonjour' : '';
 
-  var dateHuman = '$date.year:$date.month:$date.day';
-  return '$res on est le $dateHuman il est $date.hour';
+  var dateHuman = '${date.year}:${date.month}:${date.day}';
+  return '$res on est le $dateHuman il est ${date.hour}:${date.minute}';
 }
